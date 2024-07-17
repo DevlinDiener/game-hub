2aafb9fa7497651fd1ccd94e403bf40faafc7155
Devlin Diener <devlin.diener@icloud.com>
2024-07-17 16:49:08 -0700

    Building game cards

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "0a9deefacadb4ea0896559c1f4ba8a2a",
+    key: "9d1bf7ee502949d0bd2cef4f97a1410c",
   },
 });
